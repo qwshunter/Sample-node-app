da3ff3a0b3ef7404d2f0ef8eca3fa2bd2a01a2f7
qwshunter <68267800+qwshunter@users.noreply.github.com>
2020-08-10 18:43:29 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ app.get('/', async (req, res) => {
    res.send('Hello World from viren piranha ')
 })
 
+app.get('/healthCheck', async (req, res) => {
+   res.send('Hello World from viren piranha healthCheck ')
+})
+
 const port = process.env.PORT || 3000;
 	app.listen(port, () => console.log(`Listening on port ..vifen`));
 

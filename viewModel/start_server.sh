7a430cef7f024a4564522aaee3b67c5c404538a5
qwshunter <68267800+qwshunter@users.noreply.github.com>
2020-08-14 01:19:07 +0530

    Update start_server.sh

--- viewModel/start_server.sh
+++ viewModel/start_server.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # sudo chmod 755 /var/www/server.js # optional
 # this will restart app/server on instance reboot
-crontab -l | { cat; echo "@reboot pm2 start /var/www/app.js -i 0 --name \"node-app\""; } | crontab -
-sudo pm2 stop node-app
+crontab -l | { cat; echo "@reboot pm2 start /var/www/app.js -i 0 --name \"Sample-node-app\""; } | crontab -
+sudo pm2 stop Sample-node-app
 # actually start the server
-sudo pm2 start /var/www/app.js -i 0 --name "node-app"
+sudo pm2 start /var/www/app.js -i 0 --name "Sample-node-app"

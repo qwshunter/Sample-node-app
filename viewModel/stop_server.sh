b17e503858ca0693664fe502562c499b9d79d394
qwshunter <68267800+qwshunter@users.noreply.github.com>
2020-08-14 01:18:10 +0530

    Update stop_server.sh

--- viewModel/stop_server.sh
+++ viewModel/stop_server.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-sudo pm2 stop node-app
+sudo pm2 stop Sample-node-app
 
 # isExistApp=`sudo pgrep pm2`
 # if [[ -n  $isExistApp ]]; then
     # sudo pm2 kill
-    # sudo pm2 stop node-app
+    # sudo pm2 stop Sample-node-app
     # you can remove the crontab as well
 # fi
 
